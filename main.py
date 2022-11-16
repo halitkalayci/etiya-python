e7af767744403cefb73542773134710791415410
halitkalayci <halitenes.kalayci@gmail.com>
2022-11-16 15:40:51 +0300

    initial

---
+++ main.py
@@ -0,0 +1,59 @@
+print("Merhaba Etiya")
+
+
+#! string = metinsel ifade
+text = "10"
+print(text)
+
+#! integer = tam sayı
+number = 10
+print(number)
+
+#! double,float,decimal = ondalıklı sayı
+dnumber = 5.3
+print(dnumber)
+
+#! matematiksel operatörler
+print(number + 5)
+print(number - 5)
+print(number * 5)
+print(number / 5)
+print(number % 3) #! bölümünden kalan sonuç
+#! matematiksel operatörler
+
+#! boolean,bool = true veya false
+isVerified = True
+
+#! mantıksal,karşılaştırma operatörleri 
+#! => her mantıksal operatör boolean değer döner
+print(1 == 2)
+print(2 != 2) 
+
+print(2 > 2)
+print(2 < 2)
+
+print(2 >= 2)
+print(2 <= 2)
+
+print(10%2==0)
+
+print(50/2 == 25)
+print(50/2 == 25.00)
+
+#! mantıksal operatörler
+
+#! string operasyonları
+text = "Merhaba Etiya"
+print(text.upper())
+print(text.lower())
+print(text.startswith("Mer"))
+print(text.endswith("Etiya"))
+
+name = "Halit"
+age= 23
+company = "Kodlamaio"
+
+#! Halit 23 yaşında Kodlamaio'da çalışıyor
+#print(name + " " + age + " yaşında " + company + "'de çalışıyor")
+print(f"{name} {age} yaşında {company}'de çalışıyor")
+#!

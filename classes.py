352edcd3a800ac4dd94baf5d73ff3b030f4a7bd0
halitkalayci <halit.kalayci@ext.etiya.com>
2022-12-19 13:32:29 +0300

    classes

---
+++ classes.py
@@ -0,0 +1,16 @@
+class Math:
+    def sum(self,a,b):
+        return a+b
+    def multiply(self,a,b):
+        return a*b
+
+# nesne ve obje farkı => 
+mathLibrary = Math() # instance oluşturma => obje oluşturma
+sum = mathLibrary.sum(10,20)
+multiply = mathLibrary.multiply(10,20)
+
+print(sum)
+print(multiply)
+
+# selenium ve pytest
+
